0149b8e51fe770b9888f148deafe5413250124a1
osenina-galina <57868167+osenina-galina@users.noreply.github.com>
2019-11-21 03:05:45 +0200

    Delete manu.js

--- js/manu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementsByClassName('header__menu--mob')[0].addEventListener('click', function(){
-        document.body.classList.toggle('header-menu-open');
-    });
-
-    document.getElementsByClassName('overlay')[0].addEventListener('click', function(){
-        document.body.classList.remove('header-menu-open');
-    });
-});
